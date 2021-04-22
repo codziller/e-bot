2b171f2d9937c67d247b638c428305a0ac62c81a
Thekodezilla <49347488+codziller@users.noreply.github.com>
2021-04-22 15:11:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ app.use(express.static(path.join(__dirname, "frontend/build")));
 app.use(cors());
 
 // index.html for all page routes
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+// });
 //}
 
 const PORT = process.env.PORT || 5000;
